5dfc19cdeefa5924b05c84f2966a6869fba254d6
samira <samira.radmaneshfar@gmail.com>
2026-01-21 21:29:25 +0100

    init project structure

---
+++ messaging-apache-kafka/src/main/java/com/tutorial/messaging_apache_kafka/MessagingApacheKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.messaging_apache_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingApacheKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingApacheKafkaApplication.class, args);
+	}
+
+}
---
+++ messaging-apache-kafka/src/test/java/com/tutorial/messaging_apache_kafka/MessagingApacheKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.messaging_apache_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingApacheKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
